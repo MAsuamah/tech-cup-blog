d6cb797e2de0f2209e229c62dba5be95084d5b68
Michelle <michey.asmah21@gmail.com>
2021-05-24 15:29:05 -0400

    main.handlebars html-new

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
