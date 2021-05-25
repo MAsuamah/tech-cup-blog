13bb51a369475ea4756c88946a301061799ecfce
Michelle <michey.asmah21@gmail.com>
2021-05-24 22:59:52 -0400

    added sessions

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
+//Requirements
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
 
+//Express/port setup
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+//Session setup
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+//Set up handlebars middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+//Middleware for routes/path/sessions
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-// turn on routes
 app.use(routes);
 
 // turn on connection to db and server
